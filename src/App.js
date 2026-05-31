00a01f072bf1872400039852bea56e4f58b46e96
Nav-100 <nrandhawa2401@gmail.com>
2026-05-31 23:47:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑"
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App() {
+  const step =1;
+  return ( <div className="steps">
+    <div ClassName="numbers">
+      <div className="active">1</div>
+      <div>2</div>
+      <div>3</div>
     </div>
-  );
-}
 
-export default App;
+    <p className="message">Step {step}: {messages[step - 1]}</p>
+
+    <div className="buttons">
+      <button style={{backgroundColor:"#7950f2", color:"#fff"}}>Previous</button>
+      <button style={{backgroundColor:"#7950f2", color:"#fff"}}>Next</button>
+    </div>
+  </div>
+  );
+
+}
